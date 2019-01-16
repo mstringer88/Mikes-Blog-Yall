eb0456e93ccee53564a66430fe059ac99fd35196
Michael Stringer <mstringer09@yahoo.com>
2019-01-16 14:49:51 -0600

    added route for contact form

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -5,11 +5,13 @@ import usersRouter from './user';
 import authRouter from './auth';
 import { isLoggedIn, tokenMiddleware } from '../middleware/auth.mw';
 import authorRouter from './authors';
+import contactRouter from './contactform';
 
 let router = Router();
 
 router.use('/auth', authRouter);
 router.use('/authors', authorRouter);
+router.use('/contact', contactRouter);
 
 router.route('*')
     .post(tokenMiddleware, isLoggedIn)
